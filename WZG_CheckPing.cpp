61ceda0b25f4834436a98dfc2f3758359bbb03ab
wzguard <38841957+wzguard@users.noreply.github.com>
2018-04-30 02:33:52 +0700

    Update WZG_CheckPing.cpp

--- WZG_CheckPing.cpp
+++ WZG_CheckPing.cpp
@@ -14,6 +14,25 @@ in loop game for loop check internet
 @WZGuard
 */
 
+
+#include <stdio.h>
+#include <iphlpapi.h>
+#include <icmpapi.h>
+#pragma comment(lib, "IPHLPAPI.lib")
+#include <wininet.h>
+#pragma comment(lib, "wininet.lib")
+
+char* strHost = "8.8.8.8";
+char SendData[] = "..................................................................";
+HANDLE hIcmpFile = IcmpCreateFile();
+IPAddr ipaddr = inet_addr(strHost);
+DWORD dwRetVal = 0;
+DWORD ReplySize = sizeof(ICMP_ECHO_REPLY) + sizeof(SendData) + 1;
+LPVOID ReplyBuffer = (VOID*)malloc(ReplySize);
+DWORD dwError = 0;
+hostent* hp = gethostbyname(strHost);
+PICMP_ECHO_REPLY pIcmpEchoReply;
+
 BOOL WZGCheckPing()
 {
 	//beta test
